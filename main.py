3c644adcaa46b6ad41efaa0a336d2ce354a66344
NoobCoderAPIS <121336713+NoobCoderAPIS@users.noreply.github.com>
2022-12-24 23:49:53 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+#Calculator written python
+num1 = int(input("First numb: ")
+num2 = int(input("Sec numb: ")
+
+#result of calculator
+result = num1 * num2;
+
+#succes
+print("The result's", (result))
